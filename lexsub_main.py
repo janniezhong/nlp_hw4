2ec859d83316c6a951489e05ef05a86d11149a12
Jannie Zhong <24399232+janniezhong@users.noreply.github.com>
2023-04-19 13:33:23 -0400

    part 3 little changes

--- lexsub_main.py
+++ lexsub_main.py
@@ -122,7 +122,7 @@ def wn_simple_lesk_predictor(context : Context) -> str:
         lexemes = syn.lemmas()
         if len(lexemes) == 1:
             if lexemes[0].name() == lemma:
-                print("here! 1")
+                #print("here! 1")
                 continue
         #print("filtered_sentence ", filtered_sentence)
         #print("definition ", definition)
@@ -164,8 +164,8 @@ def wn_simple_lesk_predictor(context : Context) -> str:
         best_synset = overlap_dict[max_intersect][0]
     else:
         best_synset_list = overlap_dict[max_intersect]
-        print("best_synset_list:", best_synset_list)
-        print("max_intersect: ", max_intersect)
+        #print("best_synset_list:", best_synset_list)
+        #print("max_intersect: ", max_intersect)
 
         max_count = -1
         # most frequent synset
